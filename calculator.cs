e51ea6558e4cded60624a0361336edb3ca56df1f
muhammadfarizramadhani <muhammadfariz@students.amikom.ac.id>
2024-03-05 12:14:54 +0700

    Penambahkan main

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1}", a, b, Penambahan(a,b));
+		Console.WriteLine("Hasil Penambahan: {0} + {1}", a, b, Pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan Apa Saja")
+		Console.Readkey();
 	}
 	static int Penambahan(int a, int b)
 	{
